ddb7f22c8e13040cc0b8f92335de08362842b884
aehamdy <ae.hamdy91@gmail.com>
2024-02-03 16:02:47 +0200

    Added the rest of cell click event

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let currentPlayer = "X";
 const NUMBER_OF_ROWS = 3;
-const truns = NUMBER_OF_ROWS ** 2;
+const turns = NUMBER_OF_ROWS ** 2;
 let turnsCounter = 0;
 
 /*
@@ -11,13 +11,49 @@ let turnsCounter = 0;
     ]
 */
 
+let board = [
+    ["_", "_", "_"],
+    ["_", "_", "_"],
+    ["_", "_", "_"],
+]
+
+const getCellPlacement = (index, numberOfRows) => {
+    const row = Math.floor(index / numberOfRows);
+    const col = index % numberOfRows;
+
+    return [row, col];
+};
+
+const checkWin = () => false;
+
+const runWinEvent = () => false;
+
+const runDrawEvent = () => false;
+
+const drawMarkInCell = (cell, currentPlayer) => {
+    cell.querySelector(".value").textContent = currentPlayer;
+    cell.classList.add(`cell--${currentPlayer}`);
+}
+
 const cellClickHandler = (event, index) => {
     const cell = event.target;
-    const row = Math.floor(index / NUMBER_OF_ROWS);
-    const col = index % NUMBER_OF_ROWS;
-    console.log({row});
-    console.log({col});
-}
+    const [row, col] = getCellPlacement(index, NUMBER_OF_ROWS);
+
+    if (board[row][col] === "_") {
+        turnsCounter++;
+        board[row][col] = currentPlayer;
+
+        drawMarkInCell(cell, currentPlayer);
+
+        if (checkWin()) {
+            runWinEvent();
+        } else {
+            turnsCounter === turns && runDrawEvent();
+
+            currentPlayer = currentPlayer === "X" ? "O" : "X";
+        }
+    }
+};
 
 const createBoard = () => {
     const container = document.querySelector(".container");
